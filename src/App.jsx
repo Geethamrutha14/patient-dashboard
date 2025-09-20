81fb407c7610f9679e85d17ed755063e88bca42e
Geethamrutha14 <geethamruthapalla4@gmail.com>
2025-09-20 19:05:04 +0530

    routing done!

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
 import React from 'react'
 import Header from './components/Header.jsx'
+import Home from './pages/Home.jsx'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Patient from './pages/Patient.jsx'
+import About from './pages/About.jsx'
+
 
 export default function App() {
   return (
-    <div className='min-h-screen bg-gray-200'>
-     <Header/>
-
-       
-    </div>
+    <Router>
+        <div className='min-h-screen bg-gray-50'>
+          <Header/>  
+          <Routes>
+            <Route path='/' element={<Home/>}/>
+            <Route path='/about' element={<About/>}/>
+            <Route path='/patients' element={<Patient/>}/>
+          </Routes>
+        </div>
+    </Router>
   )
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,27 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function Header() {
   return (
     <div >
-      <header className='flex flex-row items-center justify-between p-6 shadow-md bg-slate-300'>
-        <div className="logo flex items-center">
-            <span className='text-2xl text-slate-400 font-bold'>Jurarut</span>
-            <span className='text-2xl text-slate-500 font-bold'>Care</span>
-        </div>
+      <header className='flex flex-row items-center justify-between p-6 shadow-md bg-slate-200'>
+        <Link to='/'>
+            <div className="logo flex items-center">
+                <span className='text-2xl text-slate-400 font-bold'>Jurarut</span>
+                <span className='text-2xl text-slate-500 font-bold'>Care</span>
+            </div>
+        </Link>
         <nav>
             <ul className='flex flex-row space-x-8 text-slate-600 font-medium'>
-                <li className='hover:text-blue-500 cursor-pointer'>Home</li>
-                <li className='hover:text-blue-500 cursor-pointer'>About</li>
-                <li className='hover:text-blue-500 cursor-pointer'>Patients</li>
+                <li>
+                    <Link to='/' className='hover:text-blue-500 cursor-pointer'>Home</Link>
+                </li>
+                <li>
+                   <Link to='/about' className='hover:text-blue-500 cursor-pointer'>About</Link>
+                </li>
+                <li>
+                    <Link to='/patients' className='hover:text-blue-500 cursor-pointer'>Patients</Link>
+                </li>
             </ul>
         </nav>
       </header>
